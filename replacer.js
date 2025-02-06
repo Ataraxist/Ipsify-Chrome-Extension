310a612955b48ea8b372c1f5077dd62c865128cd
Ataraxist <CAntico@gmail.com>
2025-02-06 11:09:34 -0800

    Feature: Converted the lorem string to an object of arrays by length. Changed lookup method to get random word of sam length +/- 1. Changed Regex to be something I understand.

--- replacer.js
+++ replacer.js
@@ -4,21 +4,247 @@
 chrome.storage.local.get(['extensionEnabled'], (result) => {
   if (!result.extensionEnabled) return;
 
-  // start with a huge lorem ipsum text string saved as a string variable
-  const ipsumText = `Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum, quia dolor sit amet consectetur adipiscing velit, sed quia non numquam do eius modi tempora incididunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrumd exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? D'Quis autem vel eum irure reprehenderit, qui in ea voluptate velit esse, quam nihil molestiae consequatur, vel illum, qui dolorem eum fugiat, quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias excepturi sint, obcaecati cupiditate non provident, similique sunt in culpa, qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerudum facilis est ert expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio, cumque nihil impedit, quo minus id, quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendaus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet, ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.`;
-
+  // so my idea is instead of using a random index of lorem for the replacement words, I could use an object and pick a random replacement word of the same lendth
+  const uniqueIpsum = {
+    1: ['a'],
+    2: [
+      'ut',
+      'ab',
+      'id',
+      'et',
+      'do',
+      'Ut',
+      'ad',
+      'ex',
+      'ea',
+      'in',
+      'At',
+      'id',
+      'Et',
+    ],
+    3: [
+      'Sed',
+      'sit',
+      'rem',
+      'aut',
+      'sed',
+      'qui',
+      'non',
+      'vel',
+      'eum',
+      'quo',
+      'eos',
+      'est',
+      'ert',
+      'Nam',
+      'cum',
+      'hic',
+      'sit',
+      'eos',
+      'est',
+    ],
+    4: [
+      'unde',
+      'iste',
+      'quae',
+      'illo',
+      'Nemo',
+      'enim',
+      'quia',
+      'odit',
+      'amet',
+      'eius',
+      'modi',
+      'quis',
+      'nisi',
+      'quam',
+      'vero',
+      'odio',
+      'quos',
+      'quas',
+      'sunt',
+      'quod',
+      'sint',
+      'ipsa',
+      'sunt',
+      'esse',
+      'sint',
+      'fuga',
+    ],
+    5: [
+      'omnis',
+      'lorem',
+      'natus',
+      'error',
+      'totam',
+      'eaque',
+      'quasi',
+      'vitae',
+      'dicta',
+      'ipsam',
+      'magni',
+      'sequi',
+      'neque',
+      'porro',
+      'dolor',
+      'ullam',
+      'autem',
+      'irure',
+      'velit',
+      'nihil',
+      'nulla',
+      'iusto',
+      'atque',
+      'harum',
+      'nobis',
+      'minus',
+      'rerum',
+      'saepe',
+      'earum',
+      'alias',
+      'fugit',
+      'ipsum',
+      'velit',
+      'illum',
+      'animi',
+      'optio',
+      'culpa',
+    ],
+    6: [
+      'beatae',
+      'labore',
+      'dolore',
+      'magnam',
+      'minima',
+      "D'Quis",
+      'quidem',
+      'libero',
+      'soluta',
+      'cumque',
+      'maxime',
+      'facere',
+      'Itaque',
+      'veniam',
+      'fugiat',
+    ],
+    7: [
+      'aperiam',
+      'dolores',
+      'ratione',
+      'dolorem',
+      'numquam',
+      'tempora',
+      'aliquam',
+      'quaerat',
+      'aliquid',
+      'commodi',
+      'officia',
+      'laborum',
+      'dolorum',
+      'rerudum',
+      'facilis',
+      'placeat',
+      'debitis',
+      'tenetur',
+      'maiores',
+      'ducimus',
+      'tempore',
+      'impedit',
+      'eveniet',
+    ],
+    8: [
+      'voluptas',
+      'quisquam',
+      'nostrumd',
+      'corporis',
+      'suscipit',
+      'deleniti',
+      'deserunt',
+      'mollitia',
+      'expedita',
+      'eligendi',
+      'officiis',
+      'sapiente',
+      'nesciunt',
+      'pariatur',
+      'corrupti',
+      'possimus',
+      'delectus',
+      'repellat',
+    ],
+    9: [
+      'inventore',
+      'veritatis',
+      'voluptate',
+      'molestiae',
+      'accusamus',
+      'molestias',
+      'excepturi',
+      'obcaecati',
+      'similique',
+      'assumenda',
+      'quibusdam',
+      'doloribus',
+      'explicabo',
+    ],
+    10: [
+      'voluptatem',
+      'doloremque',
+      'architecto',
+      'aspernatur',
+      'adipiscing',
+      'blanditiis',
+      'voluptatum',
+      'cupiditate',
+      'provident,',
+      'Temporibus',
+      'voluptates',
+      'reiciendis',
+      'laudantium',
+      'voluptatem',
+      'distinctio',
+      'recusandae',
+      'incididunt',
+      'voluptatem',
+      'laboriosam',
+      'asperiores',
+    ],
+    11: [
+      'accusantium',
+      'consectetur',
+      'dignissimos',
+      'praesentium',
+      'repudiandae',
+      'consequatur',
+      'consequatur',
+      'perferendis',
+      'consequatur',
+    ],
+    12: ['perspiciatis', 'consequuntur', 'voluptatibus', 'repellendaus'],
+    13: ['reprehenderit'],
+    14: ['exercitationem', 'necessitatibus'],
+  };
   // declare a function to get a length of replacement text
   function getLorem(word) {
-    // split the ipsum text into words using regex i dont understand
-    const ipsumWords = ipsumText.split(/\s+/);
-    // generate a random index to select a word
-    const randomIndex = Math.floor(Math.random() * ipsumWords.length);
-    // get the lorem word
-    let loremWord = ipsumWords[randomIndex];
-
+    // start with tin input words length, i am shortening words longer than 14 to 14
+    const inputLength = word.length > 14 ? 14 : word.length;
+    // get the array containing words that are the same length
+    // (index length starts at 0, but string length starts at 1)
+    let sameLength = uniqueIpsum[inputLength] || [];
+    // get the array containing words that are the same length + 1
+    let oneLonger = uniqueIpsum[inputLength + 1] || [];
+    // get the array containing words that are the same length - 1
+    let oneShorter = uniqueIpsum[inputLength - 1] || [];
+    // concatenate the 3 arrays
+    let replacementWords = sameLength.concat(oneLonger).concat(oneShorter);
+    // get a random index from 1 to array length
+    const randomIndex = Math.floor(Math.random() * replacementWords.length);
+    // pull the random word out of the replacemnt word array
+    let loremWord = replacementWords[randomIndex];
+    
     // if the input word was capitalized
     if (word === word.toUpperCase()) {
-      // , capitalize the replacement word
+      // if the first word is capitalized, capitalize the replacement word
       loremWord = loremWord.toUpperCase();
       // if the first letter of the word is capitalized,
     } else if (word[0] === word[0].toUpperCase()) {
@@ -29,11 +255,6 @@ chrome.storage.local.get(['extensionEnabled'], (result) => {
     } else {
       // replace the lowercase word with a lowercase replacment word
       loremWord = loremWord.toLowerCase();
-    }
-    // truncate the replacement word to match the length of the source word if its too long (for buttons and stuff)
-    if (loremWord.length > word.length) {
-      // +1 because there are no 1 letter lorem words... seems to work ok this way.
-      loremWord = loremWord.substring(0, word.length + 1);
     }
     // return the replaced word
     return loremWord;
@@ -68,7 +289,8 @@ chrome.storage.local.get(['extensionEnabled'], (result) => {
         ) {
           node.textContent = node.textContent.replace(
             // this regex is alien to me, i cheated to get this, who the fuck knows what it is doing
-            /\b(?!\d)\w+\b/g,
+            // /\b(?!\d)\w+\b/g,
+            /\b\w+\b/g, // (I am trying aless complcated version)
             // anyhow, if we find a match, swap it with a lorem word
             getLorem
           );
